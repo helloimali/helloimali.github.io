091c1b1c311d3c52c01482b61cb7b142449cc4be
helloimali <toalisaeedmalik@gmail.com>
2018-07-25 11:54:48 -0400

    nav circles, spin in circles, trying to fix Color Shift
    remember to add lines 6 and 7 back

--- js/main.js
+++ js/main.js
@@ -123,6 +123,16 @@ $(document).ready(function(){
     easing: 'linear'
   });
 
+
+    var scle = anime({
+      targets: '.scale',
+      duration: 2000,
+      scale: 1.3,
+      opacity: 0.3,
+      direction: 'alternate',
+      loop: true
+    });
+
   var bounce = anime({
     targets: ' .bnce',
     duration: 2000,
@@ -135,38 +145,101 @@ $(document).ready(function(){
 
 
 
+  var navMove = anime({
+    targets: '.midNavCircle',
+    translateX: -65,
+    backgroundColor: '#0af',
+    /*backgroundColor: [
+        {value: '#0af'}, // Or #FFFFFF
+        {value: '#ff0000',
+        {value: '#00ff00'}
+      ], */
+    opacity: .4,
+    easing: 'linear',
+    delay: 20,
+  });
 
-  var absoluteOffset = anime.timeline();
+  var LnavMove = anime({
+    targets: '.LNavCircle',
+    backgroundColor: '#ff0000',
+    opacity: .4,
+    translateY: -10,
+    translateX: 40,
+      easing: 'linear',
+    delay: 20,
+  });
 
-  absoluteOffset
-    .add({
-      targets: '#absoluteOffset .square.el',
-      translateX: 250,
-      easing: 'easeOutExpo',
-      offset: 1000 // Starts at 1000ms of the timeline
-    })
-    .add({
-      targets: '#absoluteOffset .circle.el',
-      translateX: 250,
-      easing: 'easeOutExpo',
-      offset: 500 // Starts at 500ms of the timeline
-    })
-    .add({
-      targets: '#absoluteOffset .triangle.el',
-      translateX: 250,
-      easing: 'easeOutExpo',
-      offset: 0 // Starts at 0ms of the timeline
-    });
+  var RnavMove = anime({
+    targets: '.RNavCircle',
+    backgroundColor: '#00ff00',
+    opacity: .4,
+    translateY: 10,
+    translateX: 40,
+      easing: 'linear',
+    delay: 20,
+  });
 
-    
-  var scle = anime({
-    targets: '.scale',
-    duration: 2000,
-    scale: 1.3,
-    opacity: 0.3,
+
+
+  var shapeShift = anime.timeline({
     direction: 'alternate',
     loop: true
-  })
+  });
+
+  shapeShift
+    .add({
+      targets: '#absoluteOffset .square.navShape',
+      easing: 'easeOutExpo',
+      borderRadius: ['0em', '2em'],
+      offset: 1500 // Starts at 1000ms of the timeline
+    })
+    .add({
+     targets: '#absoluteOffset .circle.navShape',
+      easing: 'easeOutExpo',
+      borderRadius: ['0em', '2em'],
+      offset: 1000 // Starts at 500ms of the timeline
+    })
+    .add({
+      targets: '#absoluteOffset .triangle.navShape',
+      borderRadius: ['0em', '2em'],
+      easing: 'easeOutExpo',
+      offset: 500 // Starts at 0ms of the timeline
+    });
+
+
+    var colorShift = anime.timeline({
+      direction: 'alternate',
+      loop: true
+    });
+
+    colorShift
+      .add({
+        targets: '#absoluteOffset .colorMix',
+        easing: 'easeOutExpo',
+        //backgroundColor: '#0af',
+        offset: 1500 // Starts at 1000ms of the timeline
+      })
+      .add({
+       targets: '#absoluteOffset .colorMix',
+        easing: 'easeOutExpo',
+        //backgroundColor: '#ff0000',
+        offset: 1000 // Starts at 500ms of the timeline
+      })
+      .add({
+        targets: '#absoluteOffset .colorMix',
+        easing: 'easeOutExpo',
+        //backgroundColor: '#00ff00',
+        offset: 500 // Starts at 0ms of the timeline
+      });
+
+
+
+
+
+
+
+
+
 
   var lineDrawing = anime({
      targets: '#lineDrawing .lines path',
